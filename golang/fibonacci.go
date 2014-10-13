f26feb52f6273ffccebacf832c08d981d126e6b3
high5 <high525jp@gmail.com>
2014-10-13 23:21:47 +0900

    mod fibonacci

--- golang/fibonacci.go
+++ golang/fibonacci.go
@@ -3,6 +3,9 @@ package main
 
 import (
     "fmt"
+    "math"
+    //"strconv"
+    "reflect"
 )
 
 /*
@@ -11,14 +14,72 @@ import (
 * F(n) = F(n-1) + F(n-2) 
 */
 
-
-func main() {
-	fmt.Println(fib1(38))
-}
-
 func fib1(n int) int {
 	if n <= 1 {
 		return n
 	}
 	return fib1(n-1)+fib1(n-2)
 }
+
+func fib2(n int) int {
+	if n < 2 {
+		return n
+	}
+	return fib2_sub(1, 1, n)
+}
+
+func fib2_sub(a, b, c int) int {
+	if c <= 2 {
+		return a
+	}
+	return fib2_sub(a+b, a, c-1) 
+}
+
+func fib3(n int) int{
+	fib0 := 0;
+    fib1 := 1;
+    ret := 0;
+    for i :=0; i<n; i++{
+        ret = fib1
+        fib1 = fib0 + fib1;
+        fib0 = ret;
+    }
+    return ret;
+}
+
+func fib4(n float64) float64 {
+	return math.Floor(math.Pow((1+math.Sqrt(5))/2, n) / math.Sqrt(5) + 1/2)
+}
+
+/*
+function fib4($n){
+	return floor( pow((1+sqrt(5))/2, $n) / sqrt(5) + 1/2 );
+}お伺いさせていただきます。お伺いさせていただきます。
+*/
+
+func main() {
+	fmt.Println(fib1(38))
+	fmt.Println(fib2(38))
+	fmt.Println(fib3(38))
+	fmt.Println(fib4(38))
+	fmt.Printf("%g\n", math.Floor(4.4))
+	fmt.Printf("%g\n", math.Sqrt(25))
+	fmt.Printf("%g\n", math.Pow(2, 2))
+
+	fmt.Println(reflect.TypeOf(1))
+	fmt.Println(reflect.TypeOf(math.Pow(2, 2)))
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
--- php/fibonacci.php
+++ php/fibonacci.php
@@ -2,30 +2,17 @@
 
 require_once('Bench.php');
 
-
 /*
 * F(0) = 0
 * F(1) = 1
 * F(n) = F(n-1) + F(n-2) 
 */
-
-
 function fib1 ($n) {
 	if ($n <= 1) {
 		return $n;
 	}
 	return fib1($n-1) + fib1($n-2);  
 }
-
-
-//echo fib1(11) . PHP_EOL;
-
-$bench = new Bench(); 
-
-$bench->run();
-echo fib1(38) . PHP_EOL;
-echo "fib1(38) time:" . $bench->stop()->result() . PHP_EOL;
-
 
 function fib2($n) {
         if($n < 2){
@@ -34,18 +21,38 @@ function fib2($n) {
         return fib2_sub(1, 1, $n);
 }
 
-function fib2_sub($a, $b, $c){
+function fib2_sub($a, $b, $c) {
 	if ($c <= 2){ 
 		return $a; 
 	}
     return fib2_sub($a+$b, $a, $c-1);
 }
 
+function fib3($n){
+	$fib0 = 0;
+    $fib1 = 1;
+    $ret = 0;
+    for($i=0; $i<$n; $i++){
+        $ret = $fib1;
+        $fib1 = $fib0 + $fib1;
+        $fib0 = $ret;
+    }
+    return $ret;
+}
+
+function fib4($n){
+	return floor( pow((1+sqrt(5))/2, $n) / sqrt(5) + 1/2 );
+}
+
+
 $bench = new Bench(); 
 $bench->run();
-echo fib2(38) . PHP_EOL;
-echo "fib2(38) time:" . $bench->stop()->result() . PHP_EOL;
+echo fib1(38) . PHP_EOL;
+echo "fib1(38) time:" . $bench->stop()->result() . PHP_EOL;
 
+echo fib2(38) . PHP_EOL;
+echo fib3(38) . PHP_EOL;
+echo fib4(38) . PHP_EOL;
 
 
 
